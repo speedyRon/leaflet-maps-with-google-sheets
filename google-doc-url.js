0272531b5acf8d42672a287b4158af7688c540c8
speedyRon <34038145+speedyRon@users.noreply.github.com>
2020-02-19 12:45:41 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1FajBM4MlF23jPPvESBc7OZoxabqhG77MpG6eUCSXxJc/edit#gid=0';
